971c586074c052e13b46474096d59e5988ade728
borecjeborec1 <robingrundel@seznam.cz>
2022-08-21 10:24:31 +0200

    backup

--- index.js
+++ index.js
@@ -7,74 +7,61 @@ exports.generate = async function generate(arguments) {
   const year = arguments[1]
   console.log("Display " + word + " in year " + year)
   console.log("If you want to change or cancel, press CTRL+C. You have 10 seconds.")
-  setTimeout(() => {
-    console.log("starting")
-    let month = 1
-    let day = calculateStartDay(word.length, month, year)
-    let date = `${year}-${month}-${day}`
-    let splittedWord = word.split("")
-    console.log(date)
-    for (let i = 0; i < word.length; ++i) {
-      for (let j = 0; j < 49; ++j) {
-        if (!letters[splittedWord[i]][j]) {
-          for (let k = 0; k < 1; ++k) {
-            fs.writeFileSync("fileName", JSON.stringify(Math.random()))
-            execSync(`git add .`);
-            execSync(`git commit -m "${date}" --date="${date}"`);
-          }
-        } else {
-          for (let k = 0; k < 4; ++k) {
-            fs.writeFileSync("fileName", JSON.stringify(Math.random()))
-            execSync(`git add .`);
-            execSync(`git commit -m "${date}" --date="${date}"`);
-          }
-        }
-        //
-        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-          if (day == 31) {
-            month++
-            day = 1
-          }
-          else {
-            day++
-          }
-        } else if (month == 2) {
-          if (isLeapYear(year)) {
-            if (day == 29) {
-              month++
-              day = 1
-            } else {
-              day++
-            }
-          } else {
-            if (day == 28) {
-              month++
-              day = 1
-            } else {
-              day++
-            }
-          }
+  // setTimeout(() => { }, 10000)
 
-        } else {
-          if (day == 30) {
-            month++
-            day = 1
-          } else {
-            day++
-          }
+  let month = 1
+  let day = calculateStartDay(month, year)
+
+  let date = `${year}-${month}-${day}`
+
+  let splittedWord = word.split("")
+  for (let i = 0; i < word.length; ++i) {
+    console.log(splittedWord[i] + "  is done.")
+    for (let j = 0; j < 49; ++j) {
+      if (!letters[splittedWord[i]][j]) {
+        for (let k = 0; k < 1; ++k) {
+          fs.writeFileSync("fileName", JSON.stringify(Math.random()))
+          execSync(`git add .`);
+          execSync(`git commit -m "idk what am I doing" --date="${date}"`);
         }
-        date = `${year}-${month}-${day}`
+      } else {
+        for (let k = 0; k < 4; ++k) {
+          fs.writeFileSync("fileName", JSON.stringify(Math.random()))
+          execSync(`git add .`);
+          execSync(`git commit -m "idk what am I doing" --date="${date}"`);
+        }
       }
-      console.log(splittedWord[i] + "  is done.")
+      if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+        if (day == 31) {
+          month++
+          day = 1
+        }
+        else {
+          day++
+        }
+      } else if (month == 2) {
+        if (day == 28) {
+          month++
+          day = 1
+        } else {
+          day++
+        }
+      } else {
+        if (day == 30) {
+          month++
+          day = 1
+        } else {
+          day++
+        }
+      }
+      date = `${year}-${month}-${day}`
     }
-    console.log("Deploying to github")
-    execSync(`git push`);
-    console.log("Done")
-  }, 10)
+  }
+  execSync(`git push`);
 }
 
 
-function calculateStartDay(wordLength, month, year) {
+function calculateStartDay(month, year) {
   let day = 1
   while (whatDay(day, month, year) != 0) {
     day++
