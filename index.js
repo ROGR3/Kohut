8a3370d0d3be57e630b2a1fd40e7839c5b0fa00d
borecjeborec1 <robingrundel@seznam.cz>
2022-08-21 08:44:21 +0200

    cancel timeout

--- index.js
+++ index.js
@@ -11,58 +11,60 @@ exports.generate = async function generate(arguments) {
   }
   console.log("Display " + word + " in year " + year)
   console.log("If you want to change or cancel, press CTRL+C. You have 10 seconds.")
-  setTimeout(() => { }, 10000)
-  let randomContent = JSON.stringify(Math.random())
-  let month = 1
-  let day = calculateStartDay(word.length, month, year)
-  let date = `${year}-${month}-${day}`
+  setTimeout(() => {
+    console.log("starting")
+    let randomContent = JSON.stringify(Math.random())
+    let month = 1
+    let day = calculateStartDay(word.length, month, year)
+    let date = `${year}-${month}-${day}`
 
-  let splittedWord = word.split("")
-  for (let i = 0; i < word.length; ++i) {
-    console.log(splittedWord[i] + "  is done.")
-    for (let j = 0; j < 49; ++j) {
-      if (!letters[splittedWord[i]][j]) {
-        for (let k = 0; k < 1; ++k) {
-          fs.writeFileSync("fileName", randomContent)
-          execSync(`git add .`);
-          execSync(`git commit -m "${date}" --date="${date}"`);
-        }
-      } else {
-        for (let k = 0; k < 4; ++k) {
-          fs.writeFileSync("fileName", JSON.stringify(Math.random()))
-          execSync(`git add .`);
-          execSync(`git commit -m "${date}" --date="${date}"`);
-        }
-      }
-      if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-        if (day == 31) {
-          month++
-          day = 1
-        }
-        else {
-          day++
-        }
-      } else if (month == 2) {
-        if (day == 28) {
-          month++
-          day = 1
+    let splittedWord = word.split("")
+    for (let i = 0; i < word.length; ++i) {
+      console.log(splittedWord[i] + "  is done.")
+      for (let j = 0; j < 49; ++j) {
+        if (!letters[splittedWord[i]][j]) {
+          for (let k = 0; k < 1; ++k) {
+            fs.writeFileSync("fileName", randomContent)
+            execSync(`git add .`);
+            execSync(`git commit -m "${date}" --date="${date}"`);
+          }
         } else {
-          day++
+          for (let k = 0; k < 4; ++k) {
+            fs.writeFileSync("fileName", JSON.stringify(Math.random()))
+            execSync(`git add .`);
+            execSync(`git commit -m "${date}" --date="${date}"`);
+          }
         }
-      } else {
-        if (day == 30) {
-          month++
-          day = 1
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+          if (day == 31) {
+            month++
+            day = 1
+          }
+          else {
+            day++
+          }
+        } else if (month == 2) {
+          if (day == 28) {
+            month++
+            day = 1
+          } else {
+            day++
+          }
         } else {
-          day++
+          if (day == 30) {
+            month++
+            day = 1
+          } else {
+            day++
+          }
         }
+        date = `${year}-${month}-${day}`
       }
-      date = `${year}-${month}-${day}`
     }
-  }
-  console.log("Deploying to github")
-  execSync(`git push`);
-  console.log("Done")
+    console.log("Deploying to github")
+    execSync(`git push`);
+    console.log("Done")
+  }, 10000)
 }
 
 
